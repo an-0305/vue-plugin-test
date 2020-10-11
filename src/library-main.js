8316d1b42732fff3d3d36d468af5855f571b5838
akari nakamura <akarinakamura0305@gmail.com>
2020-10-11 18:14:01 +0900

    書き方修正

--- src/library-main.js
+++ src/library-main.js
@@ -1,16 +1,31 @@
-import component from "@/components/Button.vue";
-import component2 from "@/components/Button2.vue";
+import MyButton from "@/components/Button.vue";
+import MyButton2 from "@/components/Button2.vue";
 
 const components = {
-  component,
-  component2
+  MyButton,
+  MyButton2
 };
 
-function install(Vue) {
-  Vue.component("MyButton", component);
-  Vue.component("MyButton2", component2);
+export function install(Vue) {
+  if (install.installed) return;
+  install.installed = true;
+  Vue.component("MyButton", MyButton);
+  Vue.component("MyButton2", MyButton2);
 }
 
-export { component, component2, install };
+const plugin = {
+  install
+};
+
+// vue が見つかった場合に自動インストールする (ブラウザで <script> タグを用いた場合等)
+let GlobalVue = null;
+if (typeof window !== "undefined") {
+  GlobalVue = window.Vue;
+} else if (typeof global !== "undefined") {
+  GlobalVue = global.Vue;
+}
+if (GlobalVue) {
+  GlobalVue.use(plugin);
+}
 
 export default components;
